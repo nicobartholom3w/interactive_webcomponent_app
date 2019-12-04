858ded34cae19bd9726764b4e42edad5fd2e543d
Nicole Bartholomew <nabartholomew@gmail.com>
2019-12-04 16:21:51 -0600

    (feat) Added styling to radio-button and static-radio-buttons components.

--- src/app/challenges/static-radio-buttons/buttons.service.ts
+++ src/app/challenges/static-radio-buttons/buttons.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ButtonsService {
-
+  buttons: String[] = ["sort by price", "sort by name", "sort by relevance"];
   constructor() { }
+
+
 }
