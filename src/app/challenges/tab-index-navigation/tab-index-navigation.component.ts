f2bc83333af240052c6876e1191c25e5dd06193c
Nicole Bartholomew <nabartholomew@gmail.com>
2020-01-13 12:18:15 -0600

    (feat) Added form functionality to input and submit. Tabs can now be selected.

--- src/app/challenges/tab-index-navigation/tab-index-navigation.component.ts
+++ src/app/challenges/tab-index-navigation/tab-index-navigation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TabIndexNavigationService } from './tab-index-navigation.service';
 import { Tab } from './tab';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tab-index-navigation',
@@ -10,15 +11,39 @@ import { Tab } from './tab';
 export class TabIndexNavigationComponent implements OnInit {
   index: number;
   tabsArr: Tab[] = [
-
+    {index: 1, name: "positive", text: "Positive content", active: false},
+    {index: 2, name: "negative", text: "Negative content", active: false},
+    {index: 3, name: "neutral", text: "Neutral content", active: false}
   ];
+
   constructor(private tabIndexService: TabIndexNavigationService) { }
 
   ngOnInit() {
    
   }
 
-  tabClicked(tab: Tab) {
-    
+  onSubmit(f: NgForm) {
+    if(f.status === "INVALID" || f.value.index <= 0) {
+      alert('Index is invalid.');
+    }
+    // else {
+      this.changeTab(f.value.index);
+    // }
   }
+
+  onTabSelect(i: number) {
+    this.changeTab(i + 1);
+  }
+
+  changeTab(tabIndex: number) {
+    for(let tab of this.tabsArr) {
+      if(tab.index !== tabIndex) {
+        tab.active = false;
+      }
+      else {
+        tab.active = !tab.active;
+      }
+    }
+  }
+
 }
