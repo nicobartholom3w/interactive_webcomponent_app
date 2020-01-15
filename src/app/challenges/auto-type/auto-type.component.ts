6295be92ab9c0620bd1425d93a3d821704900525
Nicole Bartholomew <nabartholomew@gmail.com>
2020-01-15 16:16:13 -0600

    (feat) Added function for text-typing animation.

--- src/app/challenges/auto-type/auto-type.component.ts
+++ src/app/challenges/auto-type/auto-type.component.ts
@@ -6,11 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auto-type.component.scss']
 })
 export class AutoTypeComponent implements OnInit {
-  changingText: string[] = ["design", "frontend", "backend", "testing"];
+  changingTextArr: string[] = ["design", "frontend", "backend", "testing"];
+  changingTextDisplay: string = "";
+  wordCount: number = 0;
+  letterCount: number = 0;
+  currentWord: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.typingDisplay();
   }
 
+  typingDisplay() {
+    if(this.wordCount >= this.changingTextArr.length) {
+      setTimeout(() => { return this.wordCount = 0;}, 1000);
+    }
+    this.currentWord = this.changingTextArr[this.wordCount];
+    this.changingTextDisplay += this.currentWord[this.letterCount];
+    this.letterCount++;
+    if(this.letterCount > this.currentWord.length){
+      this.wordCount++;
+      this.letterCount = 0;
+      this.changingTextDisplay = "";
+    }
+    
+    // setTimeout(this.typingDisplay.bind(this), 1000);
+    setTimeout(() => { return this.typingDisplay()}, 500);
+  }
+  
 }
