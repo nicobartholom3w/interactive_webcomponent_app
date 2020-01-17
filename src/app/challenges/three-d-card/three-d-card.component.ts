8f28c6d2fe42f8c4ddf8632c156ac93992720be7
Nicole Bartholomew <nabartholomew@gmail.com>
2020-01-17 11:59:51 -0600

    (feat) Initial solution using window innerWidth and innerHeight completed.

--- src/app/challenges/three-d-card/three-d-card.component.ts
+++ src/app/challenges/three-d-card/three-d-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-three-d-card',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./three-d-card.component.scss']
 })
 export class ThreeDCardComponent implements OnInit {
+  @ViewChild('card', {static: false}) card: ElementRef;
+  // @ViewChild('container', {static: false}) container: ElementRef;
+  // @HostListener('window:resize', ['event'])
+  @HostListener('document:mousemove', ['$event'])
+    onMouseMove(e) {
+      if(this.card !== undefined) {
+        let xHalf = window.innerWidth / 2;
+        let yHalf = window.innerHeight / 2;
+        let x = e.clientX - xHalf;
+        let y = e.clientY - yHalf;
+        this.renderer.setStyle(this.card.nativeElement, 'transform', `rotateY(${x / 30}deg) rotateX(${-y / 30}deg)`);
 
-  constructor() { }
+      }
+    }
+
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+
   }
 
+  ngOnDestroy() {}
 }
