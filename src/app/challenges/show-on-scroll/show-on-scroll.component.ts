b70e0b3872fd5203b27190635e0d553f2ce19feb
Nicole Bartholomew <nabartholomew@gmail.com>
2020-01-21 12:36:28 -0600

    (feat) Completed all functionality and styles for show-on-scroll.

--- src/app/challenges/show-on-scroll/show-on-scroll.component.ts
+++ src/app/challenges/show-on-scroll/show-on-scroll.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-show-on-scroll',
   templateUrl: './show-on-scroll.component.html',
   styleUrls: ['./show-on-scroll.component.scss']
 })
-export class ShowOnScrollComponent implements OnInit {
-  isHiddenArr: boolean[] = [false, true, true, true, true];
-  @ViewChild('element', {static: false}) element: ElementRef;
+export class ShowOnScrollComponent implements OnInit, AfterViewInit {
+  isHiddenArr: string[] = ['', '', '', '', '', '', ''];
+  elementPosition: any;
+  @ViewChildren ('element') elements: QueryList<ElementRef>;
 
   @HostListener('document:scroll', ['$event'])
   onScroll() {
-    // const componentPosition = this.el.nativeElement.offsetTop;
-    // const scrollPosition = window.pageYOffset;
-
-    for(let boolean of this.isHiddenArr) {
-      
-    }
+    this.elements.forEach((e) => this.isHidden(e));
   }
 
   constructor(public el: ElementRef) { }
 
   ngOnInit() {
+    
   }
 
+  ngAfterViewInit() {
+    this.elements.forEach((e) => this.isHidden(e));
+  }
+
+  isHidden(el: ElementRef) {
+    if(el){
+      this.elementPosition = el.nativeElement.getBoundingClientRect();
+      if((window.innerHeight - this.elementPosition.top) > 0) {
+        el.nativeElement.classList.remove('hidden');
+      }
+    }
+  }
 }
