2313e9b5439e5a55a19e150e53cd600164537799
Nicole Bartholomew <nabartholomew@gmail.com>
2020-01-30 15:33:53 -0600

    (feat) Auto focus funcitonal on form when keyup value is any character, number, of special character.

--- src/app/challenges/code-entry/code-entry.component.ts
+++ src/app/challenges/code-entry/code-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, QueryList } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./code-entry.component.scss']
 })
 export class CodeEntryComponent implements OnInit {
-  code: string[] = ['', '', '', '', '', ''];
-  constructor() { }
+  isActive: boolean[] = [true, false, false, false, false, false];
+  @ViewChildren ('input') inputs: QueryList<ElementRef>;
 
+  constructor() { }
+  // constructor(public el: ElementRef) { }
   ngOnInit() {
   }
   
+  onKey(event: any, i: number) {
+    if(event.target.value) {
+      this.isActive[i + 1] = true;
+      let inputsArr = this.inputs.toArray();
+      let inputsArrFocus = inputsArr[i + 1];
+      setTimeout(()=> inputsArrFocus.nativeElement.focus(), 0);
+    }
+    // if delete move back to prev input
+  }
+
+  onSubmit() {
+
+  }
 }
