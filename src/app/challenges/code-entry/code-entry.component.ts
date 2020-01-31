55dcff86a3cb12d3aa064505ba4de498eb447450
Nicole Bartholomew <nabartholomew@gmail.com>
2020-01-31 14:34:42 -0600

    (feat) Completed initial working solution for code-entry.

--- src/app/challenges/code-entry/code-entry.component.ts
+++ src/app/challenges/code-entry/code-entry.component.ts
@@ -8,24 +8,53 @@ import { NgForm } from '@angular/forms';
 })
 export class CodeEntryComponent implements OnInit {
   isActive: boolean[] = [true, false, false, false, false, false];
+  result: string[] = [];
   @ViewChildren ('input') inputs: QueryList<ElementRef>;
 
   constructor() { }
-  // constructor(public el: ElementRef) { }
-  ngOnInit() {
-  }
+  ngOnInit() {}
   
   onKey(event: any, i: number) {
-    if(event.target.value) {
+    let inputsArr = this.inputs.toArray();
+    let inputsArrFocus: ElementRef;
+    if(event.target.value && i !== inputsArr.length - 1) {
+      this.isActive[i] = false;
       this.isActive[i + 1] = true;
-      let inputsArr = this.inputs.toArray();
-      let inputsArrFocus = inputsArr[i + 1];
-      setTimeout(()=> inputsArrFocus.nativeElement.focus(), 0);
+      
+      inputsArrFocus = inputsArr[i + 1];
     }
     // if delete move back to prev input
+    if(event.key === 'Backspace' && i !== 0) {
+      this.isActive[i - 1] = true;
+      this.isActive[i] = false;
+      inputsArrFocus = inputsArr[i - 1];
+      let inputsDeleted = inputsArr[i];
+      inputsDeleted.nativeElement.value = "";
+      setTimeout(()=> inputsArrFocus.nativeElement.value = "", 0);  
+    }
+    setTimeout(()=> inputsArrFocus.nativeElement.focus(), 0);
   }
 
-  onSubmit() {
+  selectActiveInput() {
+    let inputsArr = this.inputs.toArray();
+    for(let i = 0; i < this.isActive.length; i++) {
+      if(this.isActive[i]) {
+        inputsArr[i].nativeElement.focus();
+      }
+    }
+  }
 
+  
+  onSubmit(form) {
+  let inputsArr = this.inputs.toArray();
+  let current;
+  for(let i = 0; i < this.isActive.length; i++) {
+    current = inputsArr[i];
+    if(current.nativeElement.value !== '') {
+      this.result.push(current.nativeElement.value);
+    }
+  }
+  alert(`Value is ${this.result.join('')}`);
+  this.result = [];
   }
 }
--- src/app/challenges/code-entry/code-entry.module.ts
+++ src/app/challenges/code-entry/code-entry.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeEntryComponent } from './code-entry.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +11,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [CodeEntryComponent]
 })
