4624a56361b10c43faca1c226b9668ecb6e6bf77
Nicole Bartholomew <nabartholomew@gmail.com>
2020-02-19 12:23:56 -0600

    (feat) Completed initial solution for validation-form.

--- src/app/challenges/validation-form/validation-form.component.ts
+++ src/app/challenges/validation-form/validation-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-validation-form',
@@ -7,16 +8,63 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./validation-form.component.scss']
 })
 export class ValidationFormComponent implements OnInit {
-  validationForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
+  validationForm = this.formBuilder.group({
+    name: new FormControl('', { 
+      validators: [Validators.required],
+      updateOn: 'submit' }),
+    email: new FormControl('', { 
+      validators: [Validators.email],
+      updateOn: 'submit' }),
+    password: new FormControl('', { 
+      validators: [Validators.required],
+      updateOn: 'submit' }),
+    confirmPassword: new FormControl('', { 
+      validators: [],
+      updateOn: 'submit' }),
   });
+  
+  isSubmitted: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.validationForm.controls.name);
+    this.isSubmitted = true;
+  }
+
+  isInvalidEmail() {
+    return this.validationForm.controls.email.invalid;
+  }
+
+  blankName() {
+    if(this.isSubmitted) {
+      return this.validationForm.controls.name.invalid;
+    }
+    else {
+      return false;
+    }
+  }
+
+  blankPassword() {
+    if(this.isSubmitted) {
+      return this.validationForm.controls.password.invalid;
+    }
+    else {
+      return false;
+    }
+  }
+
+  passwordMissmatch() {
+    if(this.isSubmitted && this.validationForm.controls.password.valid) {
+      if(this.validationForm.controls.password.value === this.validationForm.controls.confirmPassword.value) {
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+  }
 }
