faa946a0ee189092b71393fa527703554e7bc21a
Nicole Bartholomew <nabartholomew@gmail.com>
2020-03-06 12:18:46 -0600

    (feat) Completed initial solution for dot-container module/component.

--- src/app/challenges/dot-container/dot-container.component.ts
+++ src/app/challenges/dot-container/dot-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener, AfterViewInit } from '@angular/core';
 import { style } from '@angular/animations';
 
 @Component({
@@ -10,13 +10,22 @@ export class DotContainerComponent implements OnInit {
   @ViewChild('card', {static: false}) card: ElementRef;
   @ViewChild('dot', {static: false}) dot: ElementRef;
   @HostListener('document:keydown', ['$event'])
+  @HostListener('document:keyup', ['$event'])
     onDomEvent(event: any) {
-      this.moveDot(event);
+      if(event.type === 'keydown') {
+        this.keyDown = true;
+        this.moveDot(event);
+      }
+      else {
+        this.keyDown = false;
+      }
+      
     }
-  move: number = 3;
+  move: number = 1;
   dotOffsetTop: number;
   dotOffsetLeft: number;
-
+  width: number;
+  keyDown: boolean = false;
   constructor() { }
 
   ngOnInit() {
@@ -26,24 +35,50 @@ export class DotContainerComponent implements OnInit {
     if(this.dot) {
       this.dotOffsetTop = this.dot.nativeElement.offsetTop;
       this.dotOffsetLeft = this.dot.nativeElement.offsetLeft;
+      this.width = this.card.nativeElement.clientWidth - this.dot.nativeElement.clientWidth;
     }
     switch(event.key) {
       case 'ArrowUp': 
-        this.dotOffsetTop -= this.move;
-        this.dot.nativeElement.style.top = `${this.dotOffsetTop}px`;
+        this.moveUp();
         break;
       case 'ArrowDown':
-        this.dotOffsetTop += this.move;
-        this.dot.nativeElement.style.top = `${this.dotOffsetTop}px`;
+        this.moveDown();
         break;
       case 'ArrowLeft':
-        this.dotOffsetLeft -= this.move;
-        this.dot.nativeElement.style.left = `${this.dotOffsetLeft}px`;
+        this.moveLeft();
         break;
-        case 'ArrowRight':
-          this.dotOffsetLeft += this.move;
-          this.dot.nativeElement.style.left = `${this.dotOffsetLeft}px`;
-          break;
+      case 'ArrowRight':
+        this.moveRight();
+        break;
       }
+  }
+
+  moveLeft() {
+    if(this.dotOffsetLeft > 0 && this.keyDown) {
+      this.dotOffsetLeft -= this.move;
+      this.dot.nativeElement.style.left = `${this.dotOffsetLeft}px`;
+      requestAnimationFrame(() => this.moveLeft());
+    }
+  }
+  moveRight() {
+    if(this.dotOffsetLeft < this.width && this.keyDown) {
+      this.dotOffsetLeft += this.move;
+      this.dot.nativeElement.style.left = `${this.dotOffsetLeft}px`;
+      requestAnimationFrame(() => this.moveRight());
+    }
+  }
+  moveUp() {
+    if(this.dotOffsetTop > 0 && this.keyDown) {
+      this.dotOffsetTop -= this.move;
+      this.dot.nativeElement.style.top = `${this.dotOffsetTop}px`;
+      requestAnimationFrame(() => this.moveUp());
+    }
+  }
+  moveDown() {
+    if(this.dotOffsetTop < this.width && this.keyDown) {
+      this.dotOffsetTop += this.move;
+      this.dot.nativeElement.style.top = `${this.dotOffsetTop}px`;
+      requestAnimationFrame(() => this.moveDown());
+    }
   }
 } 
