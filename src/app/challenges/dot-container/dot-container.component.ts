4fd4432dc73cb8a7914b99ed926bb5187af1208e
Nicole Bartholomew <nabartholomew@gmail.com>
2020-03-05 18:56:20 -0600

    (feat) Basic dot movement functional in dot-container.

--- src/app/challenges/dot-container/dot-container.component.ts
+++ src/app/challenges/dot-container/dot-container.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-dot-container',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dot-container.component.scss']
 })
 export class DotContainerComponent implements OnInit {
+  @ViewChild('card', {static: false}) card: ElementRef;
+  @ViewChild('dot', {static: false}) dot: ElementRef;
+  @HostListener('document:keydown', ['$event'])
+    onDomEvent(event: any) {
+      this.moveDot(event);
+    }
+  move: number = 3;
+  dotOffsetTop: number;
+  dotOffsetLeft: number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+  moveDot(event: any) {
+    if(this.dot) {
+      this.dotOffsetTop = this.dot.nativeElement.offsetTop;
+      this.dotOffsetLeft = this.dot.nativeElement.offsetLeft;
+    }
+    switch(event.key) {
+      case 'ArrowUp': 
+        this.dotOffsetTop -= this.move;
+        this.dot.nativeElement.style.top = `${this.dotOffsetTop}px`;
+        break;
+      case 'ArrowDown':
+        this.dotOffsetTop += this.move;
+        this.dot.nativeElement.style.top = `${this.dotOffsetTop}px`;
+        break;
+      case 'ArrowLeft':
+        this.dotOffsetLeft -= this.move;
+        this.dot.nativeElement.style.left = `${this.dotOffsetLeft}px`;
+        break;
+        case 'ArrowRight':
+          this.dotOffsetLeft += this.move;
+          this.dot.nativeElement.style.left = `${this.dotOffsetLeft}px`;
+          break;
+      }
+  }
+} 
