29aed633e4726ca6589f8353e3d35c77d3b566c7
Nicole Bartholomew <nabartholomew@gmail.com>
2020-06-23 13:50:07 -0500

    (feat) Completed initial solution for onboarding.

--- src/app/challenges/onboarding/onboarding.component.ts
+++ src/app/challenges/onboarding/onboarding.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./onboarding.component.scss']
 })
 export class OnboardingComponent implements OnInit {
+  onboardingActiveArr: boolean[] = [true, false, false, false];
+  overlayActive: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onboarding(num: number) {
+    switch(num) {
+      case 0: 
+        this.overlayActive = true;
+      case 1:
+      case 2:
+        this.onboardingActiveArr[num] = false;
+        this.onboardingActiveArr[num + 1] = true;
+        break;
+      case 3:
+        this.onboardingActiveArr[num] = false;
+        this.onboardingActiveArr[0] = true;
+        this.overlayActive = false;
+    }
+  }
 }
