b604517d54ab88f4252c38407ed24571217078ad
Nicole Bartholomew <nabartholomew@gmail.com>
2020-10-08 13:44:56 -0500

    (feat) ZoomLens appears only when mouse over image and follows mouse properly.

--- src/app/challenges/image-zoom/image-zoom.component.ts
+++ src/app/challenges/image-zoom/image-zoom.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { timeStamp } from 'console';
 
 @Component({
   selector: 'app-image-zoom',
@@ -6,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-zoom.component.scss']
 })
 export class ImageZoomComponent implements OnInit {
-  
+  @ViewChild('image', {static: false}) image: ElementRef;
+  @ViewChild('zoomView', {static: false}) zoomView: ElementRef;
+  @ViewChild('zoomLens', {static: false}) zoomLens: ElementRef;
+  @ViewChild('container', {static: false}) container: ElementRef;
+  @HostListener('document:mousemove', ['$event'])
+    onMouseMove(event: MouseEvent) {
+      event.stopPropagation();
+      let imageRect = this.image.nativeElement.getBoundingClientRect();
+      this.cursorLeftDistance = event.clientX - this.lensRadiusPx;
+      this.cursorTopDistance = event.clientY - this.lensRadiusPx - this.container.nativeElement.offsetTop;
+      if(this.cursorTopDistance - this.lensRadiusPx < imageRect.top - this.imageMarginTopPx ||
+        this.cursorTopDistance - this.lensRadiusPx > imageRect.top - this.imageMarginTopPx + this.image.nativeElement.height ||
+        this.cursorLeftDistance + this.lensRadiusPx < imageRect.left ||
+        this.cursorLeftDistance + this.lensRadiusPx > imageRect.left + this.image.nativeElement.width
+        ) {
+        this.isZoom = false;
+      }
+      else {
+        this.isZoom = true;
+      }
+    }
+  isZoom: boolean = false;
+  imgSrc: string = "assets/images/image-zoom-use/image-zoom.jpg";
+  cursorLeftDistance: number;
+  cursorTopDistance: number;
+  lensDiameterPx: number = 32;
+  lensRadiusPx: number = this.lensDiameterPx/2;
+  imageMarginTopPx: number = 128;
   constructor() { }
 
   ngOnInit() {
