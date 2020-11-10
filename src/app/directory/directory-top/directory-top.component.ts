ea62d3b4529d4fe15ba00b98eca222707540ddea
Nicole Bartholomew <nabartholomew@gmail.com>
2020-11-10 11:35:56 -0600

    (feat) Added Scroll to top button to appear on scroll and scroll to top when clicked. Still need to determine when appropriate to display to top button.

--- src/app/directory/directory-top/directory-top.component.ts
+++ src/app/directory/directory-top/directory-top.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-directory-top',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directory-top.component.scss']
 })
 export class DirectoryTopComponent implements OnInit {
+  @HostListener('window:scroll', [])
+    onWindowScroll() {
+      // console.log(window.pageYOffset);
+      // console.log(document.documentElement.scrollTop);
+      if(window.pageYOffset > 100 || document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+        this.windowScrolled = true;
+      }
+      else if(this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+        this.windowScrolled = false;
+      }
+    }
+  windowScrolled: boolean = false;
+  
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  scrollToTop() {
+    (function smoothScroll() {
+      let currScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if(currScroll > 0) {
+        window.requestAnimationFrame(smoothScroll);
+        window.scrollTo(0, currScroll - (currScroll / 8));
+        console.log(currScroll);
+      }
+    })();
+  }
 }
